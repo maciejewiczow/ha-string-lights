3524a51a6d4cdad6f7991598b8d714272ad57f8b
Mafciejewicz <melonik0.9846@gmail.com>
2024-11-24 17:19:54 +0100

    Switch to neopixel fork

--- pico/install-deps.ps1
+++ pico/install-deps.ps1
@@ -5,6 +5,6 @@ param (
     $PortName = "COM6"
 )
 
-mpremote connect $PortName mip install "github:blaz-r/pi_pico_neopixel/neopixel.py"
+mpremote connect $PortName mip install "github:maciejewiczow/pi_pico_neopixel/neopixel.py"
 mpremote connect $PortName mip install "github:maciejewiczow/rpi-wifi-config"
 mpremote connect $PortName mip install "github:maciejewiczow/micropython-ha-mqtt-device"
